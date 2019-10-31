3ffef6a0e2d655a06ddb115d7d95bb054819ebc3
Evgenii <wojda007@gmail.com>
2019-10-31 20:07:21 +0200

    Updates

---
+++ precache-manifest.39e1a34d98fb34ef83e5449aa7ebb475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e7bf078c73ea40f9e2bf4e40153734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cd4b9ebfb65b76fb5e7",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2d3239178e060c55493c",
+    "url": "/static/css/main.85baa8e5.chunk.css"
+  },
+  {
+    "revision": "0cd4b9ebfb65b76fb5e7",
+    "url": "/static/js/2.ebd51e86.chunk.js"
+  },
+  {
+    "revision": "2d3239178e060c55493c",
+    "url": "/static/js/main.dfdb710d.chunk.js"
+  },
+  {
+    "revision": "95191575c9c1a0df9b74",
+    "url": "/static/js/runtime-main.d4e071dc.js"
+  }
+]);
